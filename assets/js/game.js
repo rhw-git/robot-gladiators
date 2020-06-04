8c1f92e3a0f3c3e6a84abfdceb066f7b11d23ead
rhw <ruohanw89@gmail.com>
2020-06-04 01:29:41 -0700

    add shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -81,10 +81,71 @@ var startGame = function() {
             //reset enemyHealth before starting new fight
             enemyHealth = 50;
             fight(pickedEnemyName);
+
+            // enter shop
+            var shop = function(){
+                //ask player what they'd like to do
+                var shopOptionPrompt = window.prompt(
+                    "would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+                );
+
+                //use switch to carry out action
+                switch(shopOptionPrompt){
+                    case "REFILL": // new case
+                    case "refill":
+                        if (playerMoney >7){
+                            window.alert("Refilling player's health by 20 for 7 dollars.");
+    
+                            // increase health and decrease money
+                            playerMoney = playerMoney - 7; 
+                            playerHealth = playerHealth + 20;  
+                        }else{
+                            window.alert("you don't have enough memony!");
+                        };
+                        break;
+                    
+                    case "UPGRADE":
+                    case "upgrade":
+                        if (playerMoney >7){
+                            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+    
+                            // increase attack and decrease money
+                            playerMoney = playerMoney - 7; 
+                            playerAttack = playerAttack + 6;
+                        }else{
+                            window.alert("you don't have enough memony!");
+                        };
+                        break;   
+                    
+                    case "LEAVE":
+                    case "leave":
+
+                        // do nothing, so function will end.
+                        break;
+                    
+                    default:
+                        window.alert("You did not pick a valid option. Try again.");
+
+                        // call shop function again
+                        shop();
+                };
+            };
+            
+            // if we're not at the last enemy in the array and the player still alive.
+            if (playerHealth > 0 && i < enemyNames.length - 1){
+
+                //confirm to shop
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+                if(storeConfirm){
+                    shop();
+                };
+            };
+
         }else{
             window.alert("You have lost your robot in battle! Game Over!");
             break;
         };
+
     };
 
     //function to end game
@@ -109,6 +170,8 @@ var startGame = function() {
 
     // after the loop ends, player is either out of health or enemies to fight, so run the endGame function.
     endGame();
+
+
 };
 
 // start the game when the page loads
